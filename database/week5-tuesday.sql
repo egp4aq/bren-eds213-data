d47ce45600486dab03460c9904af4f3a24619228
egp4aq <epeterson2708@gmail.com>
2025-04-29 17:35:54 +0000

    Delete statements

--- database/week5-tuesday.sql
+++ database/week5-tuesday.sql
@@ -21,3 +21,34 @@ SELECT * FROM t;
 SELECT * FROM Species s JOIN t
     ON s.Scientific_name = t.Scientific_name;
     OR (s.Scientific_name IS NULL AND t.Scientific_name IS NULL);
+
+-- inserting data
+-- this way of doing it assumes that the order of the columns is code, common name, scientific name, relevance 
+INSERT INTO Species VALUES ('abcd', 'thing', 'scientific_name', NULL);
+SELECT * FROM Species;
+-- you can explicitly label columns: this is less fragile than the first way of doing it because it explicitly tells you which names should go where
+INSERT INTO Species
+    (Common_name, Scientific_name, Code, Relevance)
+    VALUES
+    ('thing 2', 'another scientific name', 'efgh', NULL);
+SELECT * FROM Species;
+
+-- can take advantage of default values
+INSERT INTO Species
+    VALUES
+    ('thing 3', 'ijkl');
+SELECT * FROM Species;
+
+-- UPDATES and DELETES will demolish the entire table unless limited by WHERE
+DELETE FROM Bird_eggs WHERE Nest_ID
+
+-- Strategies to save yourself?
+-- Doing a SELECT first
+SELECT * FROM Bird_eggs WHERE Nest_ID LIKE 'z%';
+SELECT * FROM Bird_nests;
+
+-- try the create a copy of the table method
+CREATE TABLE nest_temp AS (SELECT * FROM Bird_nests);
+DELETE FROM nest_temp WHERE Site = 'chur';
+-- check that it worked
+SELECT * FROM nest_temp;
